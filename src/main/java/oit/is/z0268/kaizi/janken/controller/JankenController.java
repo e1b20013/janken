2551e836aede6cdba8b6895832bcc072449a4f71
e1b20013 <e1b20013@st.oit.ac.jp>
2022-10-18 10:25:11 +0900

    Janken.htmlの表示

---
+++ src/main/java/oit/is/z0268/kaizi/janken/controller/JankenController.java
@@ -0,0 +1,23 @@
+package oit.is.z0268.kaizi.janken.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class JankenController {
+
+  @GetMapping("/janken")
+  public String janken() {
+    return "janken.html";
+  }
+
+  @PostMapping("/janken")
+  public String jankenpost(@RequestParam String name, ModelMap model) {
+    model.addAttribute("yourName", name);
+    return "janken.html";
+
+  }
+}
