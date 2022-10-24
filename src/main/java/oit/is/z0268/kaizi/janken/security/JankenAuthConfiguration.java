d2e0d7b7e3117b08d489f6f190ac6a085616353a
e1b20013 <e1b20013@st.oit.ac.jp>
2022-10-24 14:56:17 +0900

    ログイン処理

---
+++ src/main/java/oit/is/z0268/kaizi/janken/security/JankenAuthConfiguration.java
@@ -0,0 +1,73 @@
+package oit.is.z0268.kaizi.janken.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+
+public class JankenAuthConfiguration {
+
+  @Bean
+  public InMemoryUserDetailsManager userDetailsService() {
+    // このクラスの下部にあるPasswordEncoderを利用して，ユーザのビルダ（ログインするユーザを作成するオブジェクト）を作成する
+    UserBuilder users = User.builder();
+
+    // UserBuilder usersにユーザ名，パスワード，ロールを指定してbuildする
+    // このときパスワードはBCryptでハッシュ化されている．
+    // ハッシュ化されたパスワードを得るには，この授業のbashターミナルで下記のように末尾にユーザ名とパスワードを指定すると良い(要VPN)
+    // $ sshrun htpasswd -nbBC 10 user1 p@ss
+    UserDetails user1 = users
+        .username("user1")
+        .password("$2y$10$LuR6R8URwx/Mlcz7YX52JefBxPaOSda26FfCl9bfx3Zf8Xf0E5SkK")
+        .roles("USER")
+        .build();
+    UserDetails user2 = users
+        .username("user2")
+        .password("$2y$10$pOdYAcoX6mKTpExMNQzoMOas/sMmmFi5rP1mFA9Kq2A6f.NMS8euG")
+        .roles("USER")
+        .build();
+    // 生成したユーザをImMemoryUserDetailsManagerに渡す（いくつでも良い）
+    return new InMemoryUserDetailsManager(user1, user2);
+  }
+
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    // Spring Securityのフォームを利用してログインを行う（自前でログインフォームを用意することも可能）
+    http.formLogin();
+
+    http.authorizeHttpRequests()
+        .mvcMatchers("/janken/**").authenticated();
+
+    http.logout().logoutSuccessUrl("/"); // ログアウト時は "http://localhost:8000/" に戻る
+
+    /**
+     * 以下2行はh2-consoleを利用するための設定なので，開発が完了したらコメントアウトすることが望ましい
+     * CSRFがONになっているとフォームが対応していないためアクセスできない
+     * HTTPヘッダのX-Frame-OptionsがDENYになるとiframeでlocalhostでのアプリが使えなくなるので，H2DBのWebクライアントのためだけにdisableにする必要がある
+     */
+    http.csrf().disable();
+    http.headers().frameOptions().disable();
+    return http.build();
+
+  }
+
+  /**
+   *
+   * UserBuilder users = User.builder();で利用するPasswordEncoderを指定する．
+   *
+   * @return BCryptPasswordEncoderを返す
+   */
+  @Bean
+  PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+}
